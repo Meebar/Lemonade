1eb03610e08ebf32800ddf584fd625bddbc7e4c9
Meebar Ranjan <meebarranjan@gmail.com>
2024-03-01 09:25:53 +0530

    2nd commit

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +16,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -28,23 +37,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Lemonade)
         setContent {
             LemonadeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                         LemonadeApp()
                 }
@@ -52,53 +64,65 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonadeApp(){
     var currentStep by remember { mutableStateOf(1) }
     var squeezeCount by remember { mutableStateOf(0) }
 
+    Scaffold (
 
-    when(currentStep){
-        1 -> LemonTextAndImage(
-            textLabelResourceId = R.string.lemon_select,
-            drawableResourceId = R.drawable.lemon_tree,
-            contentDescriptionResourceId = R.string.lemon_tree_content_description,
-            onImageClick = {
-                currentStep = 2
-                squeezeCount = (6 .. 8).random()
-            })
-        2 ->
-            LemonTextAndImage(
-                textLabelResourceId = R.string.lemon_squeeze,
-                drawableResourceId = R.drawable.lemon_squeeze,
-                contentDescriptionResourceId = R.string.lemon_content_description,
-                onImageClick = {
-                    squeezeCount--
-                    if(squeezeCount == 0){
-                        currentStep = 3
-                    }else if (squeezeCount < 0){
-                        currentStep = 0
-                    }
-                })
-        3 ->
-            LemonTextAndImage(
-                textLabelResourceId = R.string.lemon_drink,
-                drawableResourceId = R.drawable.lemon_drink,
-                contentDescriptionResourceId = R.string.lemonade_content_description,
-                onImageClick = {
-                    currentStep = 4
-                })
-        4 -> {
-            LemonTextAndImage(
-                textLabelResourceId = R.string.lemon_empty_glass,
-                drawableResourceId = R.drawable.lemon_restart,
-                contentDescriptionResourceId = R.string.empty_glass_content_description,
-                onImageClick = {
-                    currentStep = 1
+    ){
+        innerPadding ->
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+            when(currentStep){
+                1 -> LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_select,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        currentStep = 2
+                        squeezeCount = (6 .. 8).random()
+                    })
+                2 ->
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_squeeze,
+                        drawableResourceId = R.drawable.lemon_squeeze,
+                        contentDescriptionResourceId = R.string.lemon_content_description,
+                        onImageClick = {
+                            squeezeCount--
+                            if(squeezeCount == 0){
+                                currentStep = 3
+                            }else if (squeezeCount < 0){
+                                currentStep = 0
+                            }
+                        })
+                3 ->
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_drink,
+                        drawableResourceId = R.drawable.lemon_drink,
+                        contentDescriptionResourceId = R.string.lemonade_content_description,
+                        onImageClick = {
+                            currentStep = 4
+                        })
+                4 -> {
+                    LemonTextAndImage(
+                        textLabelResourceId = R.string.lemon_empty_glass,
+                        drawableResourceId = R.drawable.lemon_restart,
+                        contentDescriptionResourceId = R.string.empty_glass_content_description,
+                        onImageClick = {
+                            currentStep = 1
+                        }
+                    )
                 }
-            )
+            }
         }
     }
+
 }
 @Composable
 fun LemonTextAndImage(
@@ -108,9 +132,15 @@ fun LemonTextAndImage(
     onImageClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+
     Box(
         modifier = Modifier
     ){
+        Image(painter = painterResource(R.drawable.lemon),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.7f
+        )
         Column (
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
@@ -118,6 +148,7 @@ fun LemonTextAndImage(
         ){
             Button(onClick = onImageClick,
                 shape = RoundedCornerShape(40.dp),
+                colors = ButtonDefaults.buttonColors(Color(82, 126, 121, 255))
             ){
                 Image(
                     painter = painterResource(drawableResourceId),
@@ -132,7 +163,8 @@ fun LemonTextAndImage(
             Text(
                 text = stringResource(textLabelResourceId),
                 style = MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.SemiBold
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp
 
             )
 
@@ -143,7 +175,7 @@ fun LemonTextAndImage(
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     LemonadeTheme {
         LemonadeApp()
     }
